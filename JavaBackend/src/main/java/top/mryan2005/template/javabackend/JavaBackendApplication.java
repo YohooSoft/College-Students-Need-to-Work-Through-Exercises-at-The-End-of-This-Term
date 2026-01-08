2c2997a97f23d0c17b0c0d105da2f448f6675d4f
Yan Zhizhong <zhizhongyan@qq.com>
2026-01-08 18:03:40 +0800

    Initial commit

---
+++ JavaBackend/src/main/java/top/mryan2005/template/javabackend/JavaBackendApplication.java
@@ -0,0 +1,13 @@
+package top.mryan2005.template.javabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendApplication.class, args);
+	}
+
+}
---
+++ JavaBackend/src/test/java/top/mryan2005/template/javabackend/JavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.mryan2005.template.javabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
